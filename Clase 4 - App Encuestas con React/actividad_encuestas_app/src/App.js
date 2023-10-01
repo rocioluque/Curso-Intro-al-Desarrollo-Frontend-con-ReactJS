60e1b5a76cd3b44c57675eacbc76ef6632cdff90
rocioluque <rocioluq4@gmail.com>
2023-10-01 11:58:58 -0300

    Actividad 4 - App Encuestas con React

---
+++ Clase 4 - App Encuestas con React/actividad_encuestas_app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+// Componentes de las páginas
+import Encuestas from './components/Encuestas';
+
+function App() {
+  // Estado para rastrear las encuestas
+  const encuestas = [
+    { id: 1, pregunta: '¿Cuál es tu color favorito?', opciones:
+['Rojo', 'Azul', 'Verde'] },
+    { id: 2, pregunta: '¿Cuál es tu comida favorita?', opciones:
+['Pizza', 'Hamburguesa', 'Sushi'] },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Aplicación de Encuestas</h1>
+      
+      {/* Contenido de las paginas */}
+      <Encuestas encuestas={encuestas} />
+    </div>
+  );
+}
+export default App;
